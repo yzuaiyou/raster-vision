30827e71f98ceea795bb17e0fe4e2bd17458a8b3
Rocky Breslow <rbreslow@azavea.com>
2018-08-08 10:05:33 -0400

    Use Deep Learning Base AMI

--- deployment/packer/template.js
+++ deployment/packer/template.js
@@ -10,28 +10,21 @@
             "name": "raster-vision-gpu",
             "type": "amazon-ebs",
             "region": "{{user `aws_region`}}",
-            "availability_zone": "us-east-1e",
             "source_ami": "{{user `aws_gpu_ami`}}",
-            "instance_type": "p2.xlarge",
+            "instance_type": "p3.2xlarge",
             "ssh_username": "ec2-user",
             "ami_name": "raster-vision-gpu-{{timestamp}}-{{user `branch`}}",
             "run_tags": {
                 "PackerBuilder": "amazon-ebs"
             },
-             "launch_block_device_mappings": [
-            {
-              "device_name": "/dev/xvda",
-              "volume_size": 120,
-              "volume_type": "gp2",
-              "delete_on_termination": true
-            },
-            {
-              "device_name": "/dev/xvdcz",
-              "volume_size": 22,
-              "volume_type": "gp2",
-              "delete_on_termination": true
-            }
-          ],
+            "ami_block_device_mappings": [
+                 {
+                     "delete_on_termination": true,
+                     "device_name": "/dev/xvda",
+                     "volume_size": 120,
+                     "volume_type": "gp2"
+                 }
+            ],
             "tags": {
                 "Name": "raster-vision-gpu",
                 "Version": "{{user `raster_vision_gpu_version`}}",
@@ -40,16 +33,19 @@
             "associate_public_ip_address": true
         }
     ],
+    "_comment": "Steps below are intended to reset ECS agent state.",
     "provisioners": [
         {
             "type": "shell",
-            "script": "./packer/install-nvidia-drivers.sh",
-            "expect_disconnect": true
+            "script": "./packer/configure-gpu.sh"
         },
         {
             "type": "shell",
-            "script": "./packer/install-nvidia-docker.sh",
-            "pause_before": "30s"
+            "inline": [
+                "sleep 5",
+                "sudo stop ecs",
+                "sudo rm -rf /var/lib/ecs/data/ecs_agent_data.json"
+            ]
         }
     ]
 }
